e33a4d3469107e9fcd23bde86a91171182a896ab
Ilsaf Khasanov <ilsaf.khasanov@mail.ru>
2020-02-06 23:02:34 +0300

    wrote a config file for webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+module.exports = {
+  mode: 'production',
+  context: path.resolve(__dirname, 'src'),
+  entry: './js/main.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new OptimizeCssAssetsPlugin({
+      })
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {},
+        },
+        'css-loader',
+      ]
+    }]
+  }
+}
