f141e175d41a88a920c12e44597f19e970100cde
lulldev <lulldev@yandex.ru>
2020-02-25 14:24:48 +0300

    Fix formatting

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: './main.js',
